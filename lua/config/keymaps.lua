910e77a91dbba4c9b7daa975a418c64ab7b8470e
Matvei Smirnov <matt@Matveis-MacBook-Pro.attlocal.net>
2025-08-09 23:59:44 -0500

    keymaps upd for :q

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+pcall(vim.keymap.del, "n", "<leader>qq")
+pcall(vim.keymap.del, "n", "<leader>qa")
+
+-- <leader>q = :q
+vim.keymap.set("n", "<leader>q", "<cmd>close<cr>", { desc = "Close split", silent = true, nowait = true })
